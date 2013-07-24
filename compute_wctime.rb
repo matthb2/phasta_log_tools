0c8c841ace383db7a1452e0148d6ef03890ceac0
Benjamin Matthews <ben@kc2vjw.com>
2013-07-23 23:02:29 -0600

    Compute time based on first available timestep rather than timestep 2

--- compute_wctime.rb
+++ compute_wctime.rb
@@ -3,10 +3,14 @@ require_relative 'parse_phasta.rb'
 
 data = read_phasta_log(File.open(ARGV[0],"r"))
 
-wctime_start = data["2"].first.wctime
+first_ts = data.keys[1]
+
+wctime_start = data[first_ts].first.wctime
 
 last_ts = 0
 data.each_key {|k| last_ts = k.to_i if last_ts.to_i < k.to_i} #probably paranoid
 
 #puts data[last_ts.to_s].last.inspect
 puts data[last_ts.to_s].last.wctime - wctime_start
+
+puts "for #{data.keys.length-1} timesteps"
